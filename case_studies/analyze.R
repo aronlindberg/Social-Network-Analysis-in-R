e0404e46db36360f8e1c9cd363d27948799a186f
Aron Lindberg <aron.lindberg@case.edu>
2013-05-29 11:37:53 -0400

    open file

--- case_studies/analyze.R
+++ case_studies/analyze.R
@@ -1,6 +1,8 @@
 # Setup
-setwd("~/github/local/Social-Network-Analysis-in-R/case_studies/")
+setwd("~/github/local/Social-Network-Analysis-in-R/data/django_django/")
 # turn on the command below to output to a text-file.
 # sink("outfile.txt")
 library(igraph)
 
+# open data
+read.graph("django_django_2012_05.graphml", format = "graphml")
