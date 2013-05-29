e51763eb97d455f40291252e46ab88ea8ba8f877
Aron Lindberg <aron.lindberg@case.edu>
2013-05-29 11:00:12 -0400

    clean slate 2

--- case_studies/analyze.R
+++ case_studies/analyze.R
@@ -2,7 +2,6 @@ setwd("~/github/local/Social-Network-Analysis-in-R/case_studies/")
 # source("http://sna.stanford.edu/setup.R")
 
 sink("outfile.txt")
-readLines("http://sna.stanford.edu/setup.R")
 sink()
 
 library(igraph)
